bbf66d0e68f0f02bccdb7cd1e5130c05f5e9800b
c-altmeyer <christian@altmeyer.cc>
2015-06-20 13:03:54 +0200

    Some proceced

--- Intro to Data Science/Lesson2/2_write_your_own_simple_query.py
+++ Intro to Data Science/Lesson2/2_write_your_own_simple_query.py
@@ -7,7 +7,7 @@ Created on Thu Jun 18 17:47:31 2015
 
 import pandas
 import pandasql
-
+ 
 def select_first_50(filename):
     # Read in our aadhaar_data csv to a pandas dataframe.  Afterwards, we rename the columns
     # by replacing spaces with underscores and setting all characters to lowercase, so the
@@ -24,13 +24,11 @@ def select_first_50(filename):
     q = """
     -- YOUR QUERY HERE
     """
-    print aadhaar_data
+    #print aadhaar_data
 
     #Execute your SQL command against the pandas frame
     aadhaar_solution = pandasql.sqldf(q.lower(), locals())
-    pandasql.sqldf(aadhaar_data)
-    print("SELECT * FROM aadhaar_data;", locals())
+    print pandasql.sqldf("SELECT registrar, enrolment_agency FROM aadhaar_data LIMIT 50;", locals())
     return aadhaar_solution
-
 
 select_first_50("C:\Users\\altmeyer\Documents\GitHub\Udacity\Intro to Data Science\intro_to_ds_programming_files\lesson_2\\basic_sql_quiz\\aadhaar_data.csv")
